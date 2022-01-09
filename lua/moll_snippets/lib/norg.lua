939b5120bf4f0885d9aa5ff2070b13ef6d09a091
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-01-09 05:00:39 -0500

    neorg more snippets

--- lua/moll_snippets/lib/norg.lua
+++ lua/moll_snippets/lib/norg.lua
@@ -20,12 +20,101 @@ local events = require("luasnip.util.events")
 
 local utils = require("moll_snippets.utils")
 
+-- link to spec
+-- https://github.com/nvim-neorg/neorg/blob/main/docs/NFF-0.1-spec.md#tags
+
+-- TODO: add snippets for these.
+--     - [ ] Add more literate programming features
+--      ~> [another cool plugin](https://github.com/danymat/neogen)
+
+
 return {
     norg = {
-        s("norg code", {
+        -- make this more modular so that you can modify the @{...} ...., and then the code...
+        -- rename this to "neorg tags"
+        s("norg code", { -- neorg tag
             t({ "@code", "" }),
+            -- insert code, text, " ", insert lang
             i(1, "code goes here.."),
             t({ "", "@end"}),
         }),
+
+        s("neorg checkbox", {
+            t("- [ ] "), i(1, "todo.."),
+        }),
+
+        -------------------------
+        ---       links       ---
+        -------------------------
+
+        s("neorg link curly", {
+            t("{"), i(1, "name"), t("}["),  i(2, "link"), t("]"),
+        }),
+        s("neorg link paren", {
+            t("("), i(1, "name"), t(")["),  i(2, "link"), t("]"),
+        }),
+        s("neorg link square", {
+            t("["), i(1, "name"), t("]["),  i(2, "link"), t("]"),
+        }),
+        --
+
+
+        -- TODO: GTD
+        -- TODO: date
+        -- yyyy-mm-dd
+        -- TODO: gtd project
+          -- @code norg
+
+          --     #contexts §context_name§ §context_name§ ...
+          --     #time.start §date§
+          --     #time.due §date§
+          --     * §Project name§
+          --     - [ ] §Task description§
+          -- @end
+
+
+
+      -- @code norg
+      -- | §Area Of Focus name§
+      -- marker end
+      -- | _
+      -- @end
+
+
+      -- Below is an example with AOF's:
+
+      -- @code norg
+      -- | This is an AOF
+
+      -- * This is a project
+      -- - [ ] This is a task inside a project
+
+      -- * This is another project
+      -- - [ ] This is a task inside the other project
+
+      -- * This is a very large project
+      --  ** This is a heading for the project
+      --     - [ ] This is a task
+      --  ** This is another heading for the project
+      --     - [ ] This is a task
+
+      -- | _
+
+      -- This is not in an AOF
+      -- @end
+        --
+
+    -- TODO: quotes / comments
+        -- would it be possible to implement this with lua snippet dynamic
+        -- snippets?
+
+    -- TODO: table snippets
+
+    -- TODO: media images / video
+
+    -- TODO: math
+
+
+
     }
 }
