6fecf6b5439398ba9298c1392668fcd396804d18
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-09-01 13:10:45 -0400

    improved paths

--- lua/luasnip_snippets/init.lua
+++ lua/luasnip_snippets/init.lua
@@ -2,24 +2,50 @@ local ls = require("luasnip")
 
 local M = {}
 
+-- TODO: V2
+--
+--
+-- need to make filepaths into lua module paths before we insert them into the main table.
+--
+--
 -- 1. copy path utils doom system and paste below
 -- 2. put all paths path segments in table below.
 -- 3. print everything.
 -- 4. read luasnip manual again.
+--
+-- use path table to make path logic a little bit nicer looking
+
+-- USER SNIPPETS DIR
+--
+-- A. snippets in root dir can have multiple file extensions.
+-- B. snippets in ft folders only return a table return {}
+--
+--
+
+local PATHS = {
+	sep_lua = ".",
+	sep_sys = "/", -- how is this done in doom?
+	lua_all = "*.lua",
+	lua_ext = ".lua",
+	lua_pre = "lua/",
+}
 
 -- TODO: setup()
 local settings = {
-	snippets_path_user = "lua/snippets/", -- rename: default
-	snippets_path_internal = "lua/luasnip_snippets/snippets/",
-	nvim_snippets_modules = "snippets.",
-	luasnip_snippets_modules = "luasnip_snippets.snippets.",
+	defaults = {
+		snippets_path_user = "lua/snippets", -- rename: default
+		sys_path = "lua/snippets",
+		lua_path = "snippets",
+	},
+	snippets_path_internal = "lua/luasnip_snippets/snippets",
+	snippets_mod_path_internal = "luasnip_snippets.snippets",
 }
-local snippets_path_user = "lua/snippets/"
-local snippets_path_internal = "lua/luasnip_snippets/snippets/"
 
--- in-lua path prefixes
-local nvim_snippets_modules = "snippets."
-local luasnip_snippets_modules = "luasnip_snippets.snippets."
+-- local snippets_path_user = "lua/snippets/"
+-- local snippets_path_internal = "lua/luasnip_snippets/snippets/"
+-- -- in-lua path prefixes
+-- local nvim_snippets_modules = "snippets."
+-- local snippets_mod_path_internal = "luasnip_snippets.snippets."
 
 -- TODO: mv these lua utils to utils/init.lua && rename the current util file -> `util/snippets.lua`
 -- ??
@@ -64,7 +90,7 @@ end
 
 local function get_snippets_files_from_dir(path)
 	-- local nvim_snippets = vim.api.nvim_get_runtime_file(snippets_path_user .. "*.lua", true)
-	-- local luasnip_snippets = vim.api.nvim_get_runtime_file(snippets_path_internal .. "*.lua", true)
+	-- local luasnip_snippets = vim.api.nvim_get_runtime_file(settings.snippets_path_internal .. "*.lua", true)
 	-- return snippets
 end
 
@@ -84,16 +110,7 @@ local function merge_snippets_into_luasnip()
 end
 
 function M.setup(opts)
-	local snippet_source_paths
-	if opts.use then
-		-- for each use string
-		print("user snip use:", vim.inspect(opts.use))
-	else
-		-- load all snippets
-	end
-
-	print("load luasnip_snippets xxxx yyyyy")
-
+	local t_snippet_modules = {}
 	local t = {}
 
 	--
@@ -101,71 +118,71 @@ function M.setup(opts)
 	--
 
 	if opts.paths then
-		print("user snip path:", vim.inspect(opts.paths))
-
+		-- loop over user paths and collect snippet filepaths
 		for _, user_path in ipairs(opts.paths) do
-			local t_paths = vim.api.nvim_get_runtime_file(user_path .. "*.lua", true)
+			local up = PATHS.lua_pre .. user_path .. PATHS.sep_sys .. PATHS.lua_all
+			local t_paths = vim.api.nvim_get_runtime_file(up, true)
 			for _, snippets_file_path in ipairs(t_paths) do
-				table.insert(snippet_source_paths, snippets_file_path)
+				local mp = user_path .. PATHS.sep_lua .. get_file_name(snippets_file_path):sub(1, -5)
+				table.insert(t_snippet_modules, mp)
 			end
 		end
 	else
-		-- DEFAULT CASE: use `lua/snippets/`
-		table.insert(snippet_source_paths, vim.api.nvim_get_runtime_file("lua/snippets/*.lua", true))
+		-- default
+		local default_paths = vim.api.nvim_get_runtime_file(
+			settings.defaults.sys_path .. PATHS.sep_sys .. PATHS.lua_all,
+			true
+		)
+		for _, dp in ipairs(default_paths) do
+			local mp = settings.defaults.lua_path .. PATHS.sep_lua .. get_file_name(dp):sub(1, -5)
+			table.insert(t_snippet_modules, mp)
+		end
 	end
 
 	--
 	-- 2: GET INTERNAL SNIPPETS
 	--
 
-	local luasnip_snippets_paths = vim.api.nvim_get_runtime_file(snippets_path_internal .. "*.lua", true)
+	local luasnip_snippets_paths = vim.api.nvim_get_runtime_file(
+		settings.snippets_path_internal .. PATHS.sep_sys .. PATHS.lua_all,
+		true
+	)
 	for _, path in ipairs(luasnip_snippets_paths) do
-		table.insert(snippet_source_paths, path)
+		local mp = settings.snippets_mod_path_internal .. PATHS.sep_lua .. get_file_name(path):sub(1, -5)
+		table.insert(t_snippet_modules, mp)
 	end
-
-	------------
-	print("snippet_source_paths >>>", vim.inspect(snippet_source_paths))
-	------------
 
 	--
 	-- 3: COMPILE ALL SNIPETS INTO ONE LARGE SNIPPETS TABLE BY FILETYPE
 	--
 
-  -- a. for each snippets file.
-  -- b. for each filetype.
-  --
-  -- vim.tbl_deep_extend() use this to make it easier to merge all tables.
-
 	-- extend_luasnip_with_snippets()
 	-- t = insert_snippets_into_table(t, nvim_snippets_modules, nvim_snippets)
+	-- t = insert_snippets_into_table(t, snippets_mod_path_internal, luasnip_snippets)
 
-	-- t = insert_snippets_into_table(t, luasnip_snippets_modules, luasnip_snippets)
+	-- for _, mod_path in ipairs(t_snippet_modules) do
+	-- 	local sm = require(mod_path)
+	-- 	for ft, snips in pairs(sm) do
+	-- 		if t[ft] == nil then
+	-- 			t[ft] = snips
+	-- 		else
+	-- 			for _, s in pairs(snips) do
+	-- 				table.insert(t[ft], s)
+	-- 			end
+	-- 		end
+	-- 	end
+	-- end
 
-
-  -- FIX: MAKE THIS WORK AND PRINT AFTERWARDS
-	for _, snip_fpath in ipairs(paths_table) do
-		local snip_mname = get_file_name(snip_fpath):sub(1, -5)
-
-		local sm = require(modules_str .. snip_mname)
-
-		for ft, snips in pairs(sm) do
-			if t[ft] == nil then
-				t[ft] = snips
-			else
-				for _, s in pairs(snips) do
-					table.insert(t[ft], s)
-				end
-			end
-		end
-	end
+	------------
+	-- print("s table >>>", vim.inspect(t))
+	------------
 
 	--
 	-- 4: LOAD SNIPPETS INTO LUASNIP.
 	--
 
-	for ft, ft_snippets in pairs(t_all_snippets) do
-
-	end
+	-- for ft, ft_snippets in pairs(t_all_snippets) do
+	-- end
 
 	-- return t
 end
