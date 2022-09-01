eec765d6812d567c969534b075137a0240db8674
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-09-01 09:31:15 -0400

    save

--- lua/luasnip_snippets/init.lua
+++ lua/luasnip_snippets/init.lua
@@ -84,14 +84,7 @@ local function merge_snippets_into_luasnip()
 end
 
 function M.setup(opts)
-	-- is it possible to select any path.
-	--
-	-- default path
-	if opts.snippets_path_user then
-		print("user snip path:", opts.snippets_path_user)
-	else
-	end
-
+	local snippet_source_paths
 	if opts.use then
 		-- for each use string
 		print("user snip use:", vim.inspect(opts.use))
@@ -103,17 +96,78 @@ function M.setup(opts)
 
 	local t = {}
 
-	-- refactor: into function loop over all snippets passed via `snippets_paths`
-	local nvim_snippets = vim.api.nvim_get_runtime_file(snippets_path_user .. "*.lua", true)
+	--
+	-- 1: GET USER SNIPPET FILE PATHS
+	--
 
+	if opts.paths then
+		print("user snip path:", vim.inspect(opts.paths))
 
-	local luasnip_snippets = vim.api.nvim_get_runtime_file(snippets_path_internal .. "*.lua", true)
+		for _, user_path in ipairs(opts.paths) do
+			local t_paths = vim.api.nvim_get_runtime_file(user_path .. "*.lua", true)
+			for _, snippets_file_path in ipairs(t_paths) do
+				table.insert(snippet_source_paths, snippets_file_path)
+			end
+		end
+	else
+		-- DEFAULT CASE: use `lua/snippets/`
+		table.insert(snippet_source_paths, vim.api.nvim_get_runtime_file("lua/snippets/*.lua", true))
+	end
+
+	--
+	-- 2: GET INTERNAL SNIPPETS
+	--
+
+	local luasnip_snippets_paths = vim.api.nvim_get_runtime_file(snippets_path_internal .. "*.lua", true)
+	for _, path in ipairs(luasnip_snippets_paths) do
+		table.insert(snippet_source_paths, path)
+	end
+
+	------------
+	print("snippet_source_paths >>>", vim.inspect(snippet_source_paths))
+	------------
+
+	--
+	-- 3: COMPILE ALL SNIPETS INTO ONE LARGE SNIPPETS TABLE BY FILETYPE
+	--
+
+  -- a. for each snippets file.
+  -- b. for each filetype.
+  --
+  -- vim.tbl_deep_extend() use this to make it easier to merge all tables.
 
 	-- extend_luasnip_with_snippets()
-	t = insert_snippets_into_table(t, nvim_snippets_modules, nvim_snippets)
-	t = insert_snippets_into_table(t, luasnip_snippets_modules, luasnip_snippets)
+	-- t = insert_snippets_into_table(t, nvim_snippets_modules, nvim_snippets)
 
-	return t
+	-- t = insert_snippets_into_table(t, luasnip_snippets_modules, luasnip_snippets)
+
+
+  -- FIX: MAKE THIS WORK AND PRINT AFTERWARDS
+	for _, snip_fpath in ipairs(paths_table) do
+		local snip_mname = get_file_name(snip_fpath):sub(1, -5)
+
+		local sm = require(modules_str .. snip_mname)
+
+		for ft, snips in pairs(sm) do
+			if t[ft] == nil then
+				t[ft] = snips
+			else
+				for _, s in pairs(snips) do
+					table.insert(t[ft], s)
+				end
+			end
+		end
+	end
+
+	--
+	-- 4: LOAD SNIPPETS INTO LUASNIP.
+	--
+
+	for ft, ft_snippets in pairs(t_all_snippets) do
+
+	end
+
+	-- return t
 end
 
 return M
